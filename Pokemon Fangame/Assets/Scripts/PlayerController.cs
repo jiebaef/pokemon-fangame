684a515a23ce495db89b6a60a136618830f34605
Finn <zockeremail2@yahoo.de>
2020-10-03 16:46:18 +0200

    added player animation

---
+++ Pokemon Fangame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 5f;
+
+    public Rigidbody2D RigidBody;
+    public Animator animator;
+    Vector2 movement;
+    
+
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        var horizontal = "Horizontal";
+        var vertical = "Vertical";
+
+        movement.x = Input.GetAxisRaw(horizontal);
+        movement.y = Input.GetAxisRaw(vertical);
+
+        animator.SetFloat(horizontal, movement.x);
+        animator.SetFloat(vertical, movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void Move()
+    {
+        RigidBody.MovePosition(RigidBody.position + movement * MovementSpeed * Time.fixedDeltaTime);
+    }
+}
