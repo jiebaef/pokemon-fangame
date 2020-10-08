1350b076cd5f61087ac4c209bccf859977390138
Finn <zockeremail2@yahoo.de>
2020-10-08 16:18:31 +0200

    added temporary grid movement

--- Pokemon Fangame/Assets/Scripts/PlayerController.cs
+++ Pokemon Fangame/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,15 @@ public class PlayerController : MonoBehaviour
     public float MovementSpeed = 3f;
 
     public Rigidbody2D RigidBody;
-    public Animator animator;
+    public Animator Animator;
+    public Transform FuturePosition;
+
     Vector2 movement;
-    
+
+    private void Start()
+    {
+        FuturePosition.parent = null;
+    }
 
     void Update()
     {
@@ -16,7 +22,8 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Move();
+        //Move();
+        GridMove();
     }
 
     void ProcessInputs()
@@ -27,13 +34,32 @@ public class PlayerController : MonoBehaviour
         movement.x = Input.GetAxisRaw(horizontal);
         movement.y = Input.GetAxisRaw(vertical);
 
-        animator.SetFloat(horizontal, movement.x);
-        animator.SetFloat(vertical, movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        Animator.SetFloat(horizontal, movement.x);
+        Animator.SetFloat(vertical, movement.y);
+        Animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void Move()
     {
         RigidBody.MovePosition(RigidBody.position + movement * MovementSpeed * Time.fixedDeltaTime);
+    }
+
+    void GridMove()
+    {
+        RigidBody.position = Vector3.MoveTowards(transform.position, FuturePosition.position, MovementSpeed * Time.fixedDeltaTime);
+
+        if (Vector3.Distance(transform.position, FuturePosition.position) <= 0.75f)
+        {
+            FuturePosition.position = RigidBody.transform.position;
+            if (Mathf.Abs(movement.x) == 1f)
+            {
+                FuturePosition.position += new Vector3(movement.x, 0f, 0f);
+            }
+            else if (Mathf.Abs(movement.y) == 1f)
+            {
+                FuturePosition.position += new Vector3(0f, movement.y, 0f);
+            }
+        }
+
     }
 }
